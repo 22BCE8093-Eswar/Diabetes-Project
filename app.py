ebb460ffe28055436ab3f0a6abd7fc9d0cd27bb1
Eswar Reddy Boyi <eswarreddyboyi7@gmail.com>
2025-08-29 20:45:52 +0530

    requirements added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import joblib
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -22,4 +23,5 @@ def predict():
     return render_template('index.html', prediction_text=f'Prediction: {result}')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
